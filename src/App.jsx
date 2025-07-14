67176e75f590c36af323e4ec660e80f8c9b238c4
veeralakshmi51 <karthigaraj0928@gmail.com>
2025-07-14 13:13:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='container'>
+    <div className='card'>
+      <img src='src/assets/a1.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a2.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a3.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a4.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a5.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a6.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a16.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a7.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a8.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a9.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a10.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a11.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a12.png'/>
+    </div>
+    <div className='card'>
+      <img src='src/assets/a13.png'/>
+    </div>
+   </div>
+  )
+}
+
+export default App
